ee99515ac86806cc23cbcdf00edbd382605ae8a9
pinaki-GH <pinaki4frndz@gmail.com>
2026-01-01 17:17:00 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <h1>💰 Personal Financial Tracker</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
